c75e4411250f62e1581d5a26cd711b0c9c5296e1
Zach Peters <zpeters@gmail.com>
2018-10-13 16:41:00 -0500

    Added get messages shortcuts

--- lib/ex_webexteams/shortcuts.ex
+++ lib/ex_webexteams/shortcuts.ex
@@ -7,7 +7,7 @@ defmodule ExWebexteams.Api.Shortcuts do
   *Send a text message to a room*
   ```
   @room = 12345
-  send_message("my message") |> to_room_id(@room) |> post_message
+  send_message("my message") |> by_room_id(@room) |> post_message
   ```
 
   *Send a file to a user*
@@ -17,8 +17,15 @@ defmodule ExWebexteams.Api.Shortcuts do
   send_file(@file) |> to_person_email(@user) |> post_message
   ```
 
+  ## Example getting messages
+  *Get the last message for me from a room*
+  ```
+  @room = 12345
+  find_my_messages([:max, 1]) |> by_room_id(@room) |> get_messages
+  ```
   """
   @typep message :: %{String.t() => String.t()}
+  @typep options :: %{String.t() => String.t()}
 
   @doc "Send a text message with optional markdown"
   @spec send_message(String.t(), String.t()) :: message
@@ -52,9 +59,9 @@ defmodule ExWebexteams.Api.Shortcuts do
     Map.put(msg, "toPersonId", id)
   end
 
-  @doc "Send a prepared message to a room by id"
-  @spec to_room_id(message, String.t()) :: message
-  def to_room_id(msg, id) do
+  @doc "Get or Send a prepared message to a room by id"
+  @spec by_room_id(message, String.t()) :: message
+  def by_room_id(msg, id) do
     Map.put(msg, "roomId", id)
   end
 
@@ -63,5 +70,33 @@ defmodule ExWebexteams.Api.Shortcuts do
   def post_message(msg, api \\ ExWebexteams.Api) do
     json = Poison.encode!(msg)
     json |> api.post("/messages")
+  end
+
+  @doc """
+  Find messages from a room
+
+  ## Options example
+
+  Before message "1234"
+  ```
+  find_my_messages(%{"beforeMessage" => 1234})
+  ```
+
+  Max of 10 messages before a time (ISO8601 `yyyy-MM-dd'T'HH:mm:ss.SSSZ`)
+  ```
+  find_my_messages(%{"max" => 10, "before" => "2018-09-01T01:23:00.000Z"})
+  ```
+
+  """
+  @spec find_my_messages(options) :: options
+  def find_my_messages(options \\ %{}) do
+    Map.new([{"mentionedPeople", "me"}])
+    |> Map.merge(options) 
+  end
+
+  @doc "Get a messages from webex"
+  @spec get_messages(options, function()) :: {:ok, any}
+  def get_messages(options, api \\ ExWebexteams.Api) do
+     api.get("/messages", options)
   end
 end
