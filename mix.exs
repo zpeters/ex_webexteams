e6d363b33b0029f563c3e34a83bf6e2d8d1a9fd3
Zach Peters <zpeters@gmail.com>
2018-10-15 19:57:41 -0500

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExWebexteams.MixProject do
   def project do
     [
       app: :ex_webexteams,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
